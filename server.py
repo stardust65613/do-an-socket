53c8dfe531376e42e62062e2a6fbaaec1c32a868
Long2108 <149877137+Long2108@users.noreply.github.com>
2024-12-04 21:40:15 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,9 @@
-from socket import *
+import socket
+import threading
+import os
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+LIST_FILE_PATH = "list.txt"
+DOWNLOAD_PATH = "Download/"
