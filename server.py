996beb434d711e657b4414e8e6a606f7501f07e7
Long2108 <149877137+Long2108@users.noreply.github.com>
2024-12-05 01:13:38 +0700

    Update server.py

--- server.py
+++ server.py
@@ -7,3 +7,12 @@ ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
 LIST_FILE_PATH = "list.txt"
 DOWNLOAD_PATH = "Download/"
+def send_list_file(client_socket):
+    try:
+        with open(LIST_FILE_PATH, 'r') as file:
+            file_list = file.read()
+        client_socket.sendall(file_list.encode())
+        print("Send list file successfully")
+    except Exception as e:
+        print(f"Error: {e}")
+  
