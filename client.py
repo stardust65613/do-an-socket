2fcb98472f6e3920efbe5ec9203ec9955aafce30
Dang <gnadegreneyugn@gmail.com>
2024-12-05 18:55:17 +0700

    update server.py & client.py

--- client.py
+++ client.py
@@ -1,1 +1,55 @@
 from socket import *
+from threading import Thread
+import time
+
+def getListOfFile(file_name, list):
+    f = open(file_name,"rt")
+    for line in f:#Doc tung dong trong file
+        list.append(line)
+    f.close()
+
+def connect():
+    client_socket = socket(AF_INET, SOCK_STREAM)
+    client_socket.connect((HOST,PORT))
+    msg = client_socket.recv(BUFFSIZE).decode("utf8")
+    getServerFiles(msg)
+    print("Cac file co the tai: ")
+    for i in server_file:
+        print(i + "-" + str(server_file[i]) + " bytes")
+    
+def getServerFiles(msg):
+    temp = msg.split("\n")
+    temp.remove("")
+    for line in temp:
+        t = line.split(" ")
+        if server_file.get(t[0]) == None:
+            server_file[t[0]] = int(t[1])
+    
+
+        
+    
+        
+
+
+HOST = '127.0.0.1'
+PORT = 12345
+BUFFSIZE = 1024
+file_list = []
+server_file = {}
+#khoang thoi gian duyet lại file 
+interval = 5
+
+if __name__ == "__main__":
+    time1 = 0
+    a = Thread(target=connect)
+    b = Thread(target=connect)
+    a.start()
+    a.join()
+    while True: 
+        break
+        time2 = time.time()
+        #Duyet file input.txt 5s mot lan
+        if time2 - time1 >= interval:
+            Thread(target=getListOfFile("input.txt"), args =(file_list,) ).start()
+            Thread(target=getListOfFile("input.txt"), args =(file_list,) ).join()
+            time1 = time2
--- server.py
+++ server.py
@@ -1,1 +1,65 @@
 from socket import *
+from threading import Thread
+
+# Ham lay kich thuoc file (bytes)
+def getNumOfBytes(size):
+    res = 0
+    i = 0
+    while i < len(size):
+        if size[i] >= '0' and size[i] <= '9':
+            i += 1
+        else:
+            break
+    res = int(size[:i])
+    if size[i:] == "KB":
+        res = res*1024
+    elif size[i:] == "MB":
+        res = res*1024*1024
+    elif size[i:] == "GB":
+        res = res*1024*1024*1024
+    return res
+            
+        
+
+def getListOfFile(file_name):
+    list = {}
+    f = open(file_name,"rt")
+    for line in f:#Doc tung dong trong file
+        temp = line.split()
+        list[temp[0]] = getNumOfBytes(temp[1])
+    f.close()
+    return list
+
+def accept_connections():
+    client, addr = server.accept()
+    Thread(target=handle_client, args=(client,)).start()
+
+def handle_client(client): 
+    data = ""
+    for file in list_of_file:
+        # Gui list file 
+        data += file + " " + str(list_of_file[file]) + "\n"
+    client.send(bytes(data,"utf8"))
+    
+        
+
+def handle_downloading(client, file_name, chunk, offset):
+    a = 0
+    
+
+
+HOST = '127.0.0.1'
+PORT = 12345
+BUFFSIZE = 1024
+
+server = socket(AF_INET,SOCK_STREAM)
+server.bind((HOST,PORT))
+list_of_file = getListOfFile("input.txt")
+
+if __name__ == "__main__":
+    server.listen(5)
+    ACCEPT_THREAD = Thread(target=accept_connections)
+    
+    ACCEPT_THREAD.start()
+    ACCEPT_THREAD.join()
+    server.close()
